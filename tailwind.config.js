e0a46aa5408d0fbf9589273ef1e508c73a39ac06
joao-botrel <gleiserjosealves@gmail.com>
2023-02-09 19:03:19 -0300

    Finalizado o setup das cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'red': '#FEB172',
+      'blue': {
+        'dark': '#242440',
+        'light': '#548687',
+      },
+      'white': '#D9D3C5'
+    },
     extend: {},
   },
   plugins: [],
